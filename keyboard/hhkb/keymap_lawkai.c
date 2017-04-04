be90eb120291eba966ac43d2421b3929aed2fd29
Kelvin Law <kailaw@gmail.com>
2017-04-04 23:50:19 +0800

    Removed Right Shift One Shot prevention

--- keyboard/hhkb/keymap_lawkai.c
+++ keyboard/hhkb/keymap_lawkai.c
@@ -26,7 +26,7 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
     KEYMAP(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,GRV, \
            TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC, \
            FN7, A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN, QUOT,FN3, \
-           FN4, Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH, FN6,FN1, \
+           FN4, Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH, RSFT,FN1, \
                 LALT,LGUI,          FN8,                RGUI, RALT),
 
     /* Layer 1: HHKB mode[HHKB Fn]
