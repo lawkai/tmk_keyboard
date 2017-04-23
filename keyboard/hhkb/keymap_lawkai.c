a4118a720d0f3a489ff1e894583b20243d8bd712
Kelvin Law <kailaw@gmail.com>
2017-04-23 21:13:19 +0800

    Revert back ALT key

--- keyboard/hhkb/keymap_lawkai.c
+++ keyboard/hhkb/keymap_lawkai.c
@@ -27,7 +27,7 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
            TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC, \
            FN7, A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN, QUOT,FN9, \
            FN4, Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH, RSFT,FN1, \
-                LGUI,LALT,          FN8,                FN6, RGUI),
+                LGUI,LALT,          FN8,                RALT, RGUI),
 
     /* Layer 1: HHKB mode[HHKB Fn]
      * ,-----------------------------------------------------------.
@@ -45,13 +45,13 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
     [1] = \
     KEYMAP(PWR, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS, DEL, \
 //         TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,
-           CAPS,NO,  HOME,UP,  PGUP,NO,  NO,  NO,  PSCR,  SLCK,  PAUS,  UP,  NO,  FN5, \
+           CAPS,NO,  HOME,UP,  PGUP,NO,  NO,  NO,  PSCR,  SLCK,  PAUS,  UP,  NO,  BSPC, \
 //         LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN, QUOT,FN3,
            LCTL,NO,  LEFT,DOWN,RGHT,NO,  PAST,PSLS,HOME,  PGUP,  LEFT,   RGHT,  TRNS, \
 //         FN4, Z,   X,   C,   V,   B,   N,   M,   COMM, DOT,   FN2,   RSFT, FN1,
            LSFT,NO,  END, NO,  PGDN,NO,  PPLS,PMNS,  END,  PGDN,  DOWN,  RSFT, FN1, \
 //              LALT,LGUI,          FN4,                FN2, RALT),
-                TRNS,LALT,          TRNS,               TRNS,RGUI),
+                TRNS,TRNS,          TRNS,               TRNS,TRNS),
 
 
     /* Layer 2: Mouse mode(IJKL)[Space]
@@ -77,21 +77,21 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
 //         FN4, Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, FN2, RSFT,FN1,
            LSFT,NO,  NO,  NO,  NO,  NO,  WH_L, WH_D,WH_U,WH_R,DOWN,RSFT,NO, \
 //              LALT,LGUI,          FN4,                FN2, RALT),
-                TRNS,LALT,          TRNS,               TRNS,RGUI),
+                TRNS,TRNS,          TRNS,               TRNS,TRNS),
 
     [3] = \
     KEYMAP(ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,   F8,  F9,  F10,  F11,  F12,  INS,DEL, \
             TAB, Q,   W,   E,   R,   T,   Y,   U,    I,   O,    P, LBRC, RBRC, BSPC, \
-           LCTL, A,   S,   D,   F,   G,   H,   J,    K,   L, SCLN, QUOT,  FN3, \
+           LCTL, A,   S,   D,   F,   G,   H,   J,    K,   L, SCLN, QUOT,  TRNS, \
            LSFT, Z,   X,   C,   V,   B,   N,   M, COMM, DOT, SLSH, RSFT,  NO, \
-              TRNS,LALT,           SPC,           TRNS, RGUI),
+              TRNS,TRNS,           SPC,           TRNS, TRNS),
 
     [4] = \
     KEYMAP(ESC, NO,  NO,  NO, END,  NO,  NO,  NO,  NO,   NO,HOME, NO, NO, INS, DEL, \
             TAB, NO,  NO,  NO,  NO,  NO,  NO,PGUP,  NO,   NO,  NO, NO, NO, BSPC, \
            LCTL,NO,  NO,  NO,PGDN,  NO,LEFT,DOWN,  UP,RIGHT,  NO, NO, TRNS, \
            LSFT,NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,   NO,  NO,RSFT,NO, \
-                TRNS,LALT,          NO,                LCTL,  NO),
+                TRNS,TRNS,          NO,                TRNS,  TRNS),
 };
 
 
