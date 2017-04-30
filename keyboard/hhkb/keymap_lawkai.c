30d0d3e4984915c0722db19f78b5143c46e7877e
Kelvin Law <kailaw@gmail.com>
2017-04-30 17:23:13 +0800

    Change LSHIFT/RSHIFT to Single tap bracket (open/close). LGUI/RGUI Single tap to open/close curly bracket

--- keyboard/hhkb/keymap_lawkai.c
+++ keyboard/hhkb/keymap_lawkai.c
@@ -26,8 +26,8 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
     KEYMAP(FN5, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,GRV, \
            TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC, \
            FN7, A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN, QUOT,FN9, \
-           FN4, Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH, RSFT,FN1, \
-                LGUI,FN3,          FN8,                RALT, FN4),
+           FN4, Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH, FN6,FN1, \
+                FN10,FN3,          FN8,                RALT, FN11),
 
     /* Layer 1: HHKB mode[HHKB Fn]
      * ,-----------------------------------------------------------.
@@ -40,17 +40,13 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      * |Shift   |   |   |   |   |   |  +|  -|End|PgD|Dow|Shift |   |
      * `-----------------------------------------------------------'
      *       |Alt|Gui  |         Space         |Gui  |Alt|
-     *       `-------------------------------------------'
+     *       ---------'
      */
     [1] = \
     KEYMAP(PWR, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS, DEL, \
-//         TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,
            CAPS,NO,  HOME,UP,  PGUP,NO,  NO,  NO,  PSCR,  SLCK,  PAUS,  UP,  NO,  BSPC, \
-//         LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN, QUOT,FN3,
            LCTL,NO,  LEFT,DOWN,RGHT,NO,  PAST,PSLS,HOME,  PGUP,  LEFT,   RGHT,  TRNS, \
-//         FN4, Z,   X,   C,   V,   B,   N,   M,   COMM, DOT,   FN2,   RSFT, FN1,
            LSFT,NO,  END, NO,  PGDN,NO,  PPLS,PMNS,  END,  PGDN,  DOWN,  RSFT, FN1, \
-//              LALT,LGUI,          FN4,                FN2, RALT),
                 TRNS,TRNS,          TRNS,               TRNS,TRNS),
 
 
@@ -99,6 +95,9 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
 /* id for user defined functions */
 enum function_id {
     LSHIFT_LPAREN,
+    RSHIFT_RPAREN,
+    LGUI_LCURLY,
+    RGUI_RCURLY,
     CTL_LAYER_ENTER,
 };
 
@@ -122,13 +121,14 @@ const action_t fn_actions[] PROGMEM = {
     [1] = ACTION_LAYER_MOMENTARY(1),                  // HHKB layer(toggle with 5 taps)
     [2] = ACTION_LAYER_TOGGLE(4),                     // Navigation Layer (IJKL)
     [3] = ACTION_LAYER_MODS(3, MOD_LALT),             // LALT with layer 3
-    [4] = ACTION_MODS_ONESHOT(MOD_LSFT),              // Oneshot Shift (stay)
+    [4] = ACTION_FUNCTION_TAP(LSHIFT_LPAREN),         // single left shift = ( 
     [5] = ACTION_MODS_TAP_KEY(MOD_RGUI, KC_ESC),      // RGUI + ESC
-    [6] = ACTION_LAYER_MODS(3, MOD_LCTL),             // Switch Layer to 3 + Ctrl
+    [6] = ACTION_FUNCTION_TAP(RSHIFT_RPAREN),         // single right shift = )
     [7] = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_ESC),      // LControl with tap Esc
     [8] = ACTION_LAYER_TAP_KEY(2, KC_SPC),            // Mousekey layer with Space
     [9] = ACTION_FUNCTION_TAP(CTL_LAYER_ENTER),
-
+    [10] = ACTION_FUNCTION_TAP(LGUI_LCURLY),          // single left GUI = { 
+    [11] = ACTION_FUNCTION_TAP(RGUI_RCURLY),          // single right GUI = } 
 
 //  [x] = ACTION_LMOD_TAP_KEY(KC_LCTL, KC_BSPC),        // LControl with tap Backspace
 //  [x] = ACTION_LMOD_TAP_KEY(KC_LCTL, KC_ESC),         // LControl with tap Esc
@@ -211,6 +211,78 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
                 }
             }
             break;
+        case RSHIFT_RPAREN:
+            if (record->event.pressed) {
+                if (record->tap.count > 0 && !record->tap.interrupted) {
+                    if (record->tap.interrupted) {
+                        dprint("tap interrupted\n");
+                        register_mods(MOD_BIT(KC_RSHIFT));
+                    }
+                } else {
+                    register_mods(MOD_BIT(KC_RSHIFT));
+                }
+            } else {
+                if (record->tap.count > 0 && !(record->tap.interrupted)) {
+                    add_weak_mods(MOD_BIT(KC_RSHIFT));
+                    send_keyboard_report();
+                    register_code(KC_0);
+                    unregister_code(KC_0);
+                    del_weak_mods(MOD_BIT(KC_RSHIFT));
+                    send_keyboard_report();
+                    record->tap.count = 0;  // ad hoc: cancel tap
+                } else {
+                    unregister_mods(MOD_BIT(KC_RSHIFT));
+                }
+            }
+            break;        
+        case LGUI_LCURLY:
+            if (record->event.pressed) {
+                if (record->tap.count > 0 && !record->tap.interrupted) {
+                    if (record->tap.interrupted) {
+                        dprint("tap interrupted\n");
+                        register_mods(MOD_BIT(KC_LGUI));
+                    }
+                } else {
+                    register_mods(MOD_BIT(KC_LGUI));
+                }
+            } else {
+                if (record->tap.count > 0 && !(record->tap.interrupted)) {
+                    add_weak_mods(MOD_BIT(KC_LSHIFT));
+                    send_keyboard_report();
+                    register_code(KC_LBRC);
+                    unregister_code(KC_LBRC);
+                    del_weak_mods(MOD_BIT(KC_LSHIFT));
+                    send_keyboard_report();
+                    record->tap.count = 0;  // ad hoc: cancel tap
+                } else {
+                    unregister_mods(MOD_BIT(KC_LGUI));
+                }
+            }
+            break;        
+        case RGUI_RCURLY:
+            if (record->event.pressed) {
+                if (record->tap.count > 0 && !record->tap.interrupted) {
+                    if (record->tap.interrupted) {
+                        dprint("tap interrupted\n");
+                        register_mods(MOD_BIT(KC_RGUI));
+                    }
+                } else {
+                    register_mods(MOD_BIT(KC_RGUI));
+                }
+            } else {
+                if (record->tap.count > 0 && !(record->tap.interrupted)) {
+                    add_weak_mods(MOD_BIT(KC_RSHIFT));
+                    send_keyboard_report();
+                    register_code(KC_RBRC);
+                    unregister_code(KC_RBRC);
+                    del_weak_mods(MOD_BIT(KC_RSHIFT));
+                    send_keyboard_report();
+                    record->tap.count = 0;  // ad hoc: cancel tap
+                } else {
+                    unregister_mods(MOD_BIT(KC_RGUI));
+                }
+            }
+            break;        
         case CTL_LAYER_ENTER:
             if (record->event.pressed) {
                  if (record->tap.count == 0) {
